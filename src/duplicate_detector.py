b1eb2959098e351b32c92a187808e4c7306a9237
for-loop <31600246+for-loop@users.noreply.github.com>
2019-09-27 10:00:11 -0700

    init duplicate_detector
    This vers runs on local mode

---
+++ src/duplicate_detector.py
@@ -0,0 +1,68 @@
+__version__ = '0.5.0'
+
+import psycopg2
+from psycopg2 import Error
+import json
+import base64
+import hashlib
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+def debug(elem):
+    '''
+    Print element
+    '''
+    print(elem)
+    return None
+
+def encode(data, mode=1):
+    '''
+    Return encoded string 
+    0: base64 (first 50)
+    1: md5
+    '''
+    if mode == 0:
+        return base64.b64encode(data).decode()[:50]
+    else:
+        return hashlib.md5(data).hexdigest()
+
+def insert(row):
+    '''
+    Insert a row into PostgreSQL
+    '''
+    file_path = row[0]
+    data = row[1]
+    try:
+        with open("../postgres_credentials.json", "r") as read_file:
+            auth = json.load(read_file)
+        
+        connection = psycopg2.connect(user = auth['user'],
+                                      password = auth['password'],
+                                      host = auth['host'],
+                                      port = auth['port'],
+                                      database = auth['database'])
+        cursor = connection.cursor()
+
+        encoded_str = encode(data)
+        q = "INSERT INTO images (path, encoding) VALUES ('" + file_path + "', '" + encoded_str + "');"
+        cursor.execute(q)
+        connection.commit()
+    except () as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL closed")
+
+if __name__ == "__main__":
+    
+    sc = SparkContext('local')
+    spark = SparkSession(sc)
+
+    # Create image DataFrame using image data source in Apache Spark 2.4
+    image_df = spark.read.format("image").load('s3a://femto-data/test_1/')
+    
+    #image_df.select("image.origin", "image.data").foreach(debug)
+    
+    image_df.select("image.origin", "image.data").foreach(insert)
