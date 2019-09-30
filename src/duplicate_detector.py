01e558fa8ab72f34550922e0edc61fc3226d716b
for-loop <31600246+for-loop@users.noreply.github.com>
2019-09-29 18:11:36 -0700

    Add load and resize

--- src/duplicate_detector.py
+++ src/duplicate_detector.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-__version__ = '0.6.7'
+__version__ = '0.6.8'
 
 import sys
 import io
@@ -15,6 +15,39 @@ from pyspark.sql import functions as F
 from pyspark.sql.session import SparkSession
 import pgconf as pc
 
+
+def load(file_path, bucket_name, region_name):
+    '''
+    Load and return data in binary format
+    '''
+    s3 = boto3.client('s3', region_name)
+    file_obj = s3.get_object(Bucket=bucket_name, Key=file_path)
+    
+    return file_obj['Body'].read()
+
+
+def resize(file_path, bucket_name, region_name, reduce_factor=256):
+    '''
+    Load and return image data of reduced resolution
+    '''
+    s3 = boto3.resource('s3', region_name)
+    bucket = s3.Bucket(bucket_name)
+    file_obj = bucket.Object(file_path)
+    file_stream = io.BytesIO()
+    file_obj.download_fileobj(file_stream)
+    img = skio.imread(file_stream)
+    width = img.shape[0]//reduce_factor
+    height = img.shape[1]//reduce_factor
+    
+    # The following outputs a result that is close to skimage.transform.resize()
+    # with preserve_range=True and anti_aliasing=False.
+    # It is done manually instead because the installed skimage vers 0.10
+    # does not support the parameters.
+    PRESERVE_RANGE_FACTOR = 256
+    
+    return (tranform.resize(img, (width, height), mode='reflect')*PRESERVE_RANGE_FACTOR).astype(np.uint8)
+    
+    
 def encode(file_path, bucket_name, region_name='us-west-2', method='checksum'):
     '''
     Return encoded string 
@@ -22,27 +55,15 @@ def encode(file_path, bucket_name, region_name='us-west-2', method='checksum'):
     base: base64
     base_small: base64 on reduced image resolution
     '''
-    s3 = boto3.client('s3', region_name)
-    
-    file_obj = s3.get_object(Bucket=bucket_name, Key=file_path)
-    data = file_obj['Body'].read()
-    
     if method == 'checksum':
+        data = load(file_path, bucket_name, region_name)
         return hashlib.md5(data).hexdigest()
     elif method == 'base':
+        data = load(file_path, bucket_name, region_name)
         return base64.b64encode(data).decode()
     elif method == 'base_small':
-        s3_res = boto3.resource('s3', region_name)
-        bucket = s3_res.Bucket(bucket_name)
-        obj = bucket.Object(file_path)
-        file_stream = io.BytesIO()
-        obj.download_fileobj(file_stream)
-        img = skio.imread(file_stream)
-        denominator = 2**8
-        width = img.shape[0]//denominator
-        height = img.shape[1]//denominator
-        resized_img = (transform.resize(img, (width, height), mode='reflect')*256).astype(np.uint8)
-        return base64.b64encode(resized_img).decode()
+        data = resize(file_path, bucket_name, region_name)
+        return base64.b64encode(data).decode()
 
 
 if __name__ == "__main__":
